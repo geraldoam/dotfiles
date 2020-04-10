425c8a0d0a0429a2353d1d29452148e4cdced22a
i386angel <geraldoahnert@gmail.com>
2020-04-10 11:35:24 -0300

    Alterando fontes para Siji e adicionando script para ProtonVPN.

--- polybar/scripts/protonvpn/protonvpn.sh
+++ polybar/scripts/protonvpn/protonvpn.sh
@@ -3,7 +3,7 @@
 STATUS=$(protonvpn status | grep Status | tr -d ' ' | cut -d ':' -f2)
 
 if [ "$STATUS" = "Connected" ]; then
-    echo "vpn %{F#7eda63}%{A1:protonvpn c:} %{A}%{F-}"
+    echo "vpn %{F#7eda63}%{A1:protonvpn c:} %{A}%{F-}"
 else
-    echo "vpn %{F#d37272}%{A1:protonvpn c:} %{A}%{F-}"
+    echo "vpn %{F#d37272}%{A1:protonvpn c:} %{A}%{F-}"
 fi
