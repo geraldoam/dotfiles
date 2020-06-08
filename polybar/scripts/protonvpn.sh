67b87b03b67fff84489bcc32d4c4c77e9f81b923
i386angel <geraldoahnert@gmail.com>
2020-06-08 10:08:07 -0300

    Adicionando urxvt e apagando scripts inúteis.

--- polybar/scripts/protonvpn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-STATUS=$(protonvpn status | grep Status | tr -d ' ' | cut -d ':' -f2)
-
-if [ "$STATUS" = "Connected" ]; then
-    echo "%{F#7eda63}%{A1:protonvpn c:} Connected%{A}%{F-}"
-else
-    echo "%{F#d37272}%{A1:protonvpn c:} Off%{A}%{F-}"
-fi
